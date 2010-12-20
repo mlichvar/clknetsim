ae4496f63bdb9e7afeb02e338d49580087eef456
Miroslav Lichvar <mlichvar@redhat.com>
2010-12-20 12:42:01 +0100

    define some recent adjtimex constants if missing in timex.h

--- sysheaders.h
+++ sysheaders.h
@@ -13,4 +13,22 @@
 #include <math.h>
 #include <stdlib.h>
 
+#ifdef __linux__
+#ifndef ADJ_MICRO
+#define ADJ_MICRO               0x1000  /* select microsecond resolution */
+#endif
+#ifndef ADJ_NANO
+#define ADJ_NANO                0x2000  /* select nanosecond resolution */
+#endif
+#ifndef ADJ_OFFSET_SS_READ
+#define ADJ_OFFSET_SS_READ      0xa001  /* read-only adjtime */
+#endif
+#ifndef STA_NANO
+#define STA_NANO        0x2000  /* resolution (0 = us, 1 = ns) (ro) */
+#endif
+#ifndef STA_MODE
+#define STA_MODE        0x4000  /* mode (0 = PLL, 1 = FLL) (ro) */
+#endif
+#endif
+
 #endif
