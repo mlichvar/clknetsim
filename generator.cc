mlichvar/clknetsim

commit cc5173f886411b7b22b5ecc5416fec50c2a10bc6

isolate random state of generators
This improves reproducibility of simulations with a constant seed with
respect to timing and order of packets.

generator.cc
@@ -67,13 +67,20 @@ double Generator_variable::generate(const Generator_variables *variables) {
 Generator_random_uniform::Generator_random_uniform(const vector<Generator *> *input):
 	Generator(NULL) {
 	syntax_assert(!input || input->size() == 0);
+
+	initstate(random(), random_state, sizeof (random_state));
 }
 
 double Generator_random_uniform::generate(const Generator_variables *variables) {
+	char *prev_state;
 	double x;
+
+	prev_state = setstate(random_state);
 
 	x = ((random() & 0x7fffffff) + 1) / 2147483649.0;
 	x = ((random() & 0x7fffffff) + x) / 2147483648.0;
+
+	setstate(prev_state);
 
 	return x;
 }

generator.h
@@ -56,6 +56,8 @@ class Generator_variable: public Generator {
 };
 
 class Generator_random_uniform: public Generator {
+	char random_state[16];
+
 	public:
 	Generator_random_uniform(const vector<Generator *> *input);
 	virtual double generate(const Generator_variables *variables);

commit ce1addee0c81aa5f6d0005ad25808fb7a1ff0486

fix random seeding to avoid duplicated states
Fixes: cc5173f88641 ("isolate random state of generators")

generator.cc
@@ -66,9 +66,12 @@ double Generator_variable::generate(const Generator_variables *variables) {
 
 Generator_random_uniform::Generator_random_uniform(const vector<Generator *> *input):
 	Generator(NULL) {
+	char *prev_state;
+
 	syntax_assert(!input || input->size() == 0);
 
-	initstate(random(), random_state, sizeof (random_state));
+	prev_state = initstate(random(), random_state, sizeof (random_state));
+	setstate(prev_state);
 }
 
 double Generator_random_uniform::generate(const Generator_variables *variables) {