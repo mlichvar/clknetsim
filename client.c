mlichvar/clknetsim

commit 59e55cd801c32a5f66ceb7526eb4cf9e2406a6fc

add IP/IPV6_RECVERR control message to messages in error queue

client.c
@@ -62,6 +62,7 @@
 #include <linux/rtc.h>
 #include <linux/sockios.h>
 #ifdef SO_TIMESTAMPING
+#include <linux/errqueue.h>
 #include <linux/ptp_clock.h>
 #include <linux/net_tstamp.h>
 #endif
@@ -2810,6 +2811,35 @@ ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags) {
 				memcpy(CMSG_DATA(cmsg), &tpi, sizeof (tpi));
 			}
 #endif
+		}
+
+		if (last_ts_msg) {
+			struct sock_extended_err err;
+
+			cmsg = (struct cmsghdr *)((char *)CMSG_FIRSTHDR(msg) + cmsglen);
+			cmsglen += CMSG_SPACE(sizeof (err));
+			assert(msg->msg_control && msg->msg_controllen >= cmsglen);
+			switch (ip_family) {
+				case 4:
+					cmsg->cmsg_level = SOL_IP;
+					cmsg->cmsg_type = IP_RECVERR;
+					break;
+				case 6:
+					cmsg->cmsg_level = SOL_IPV6;
+					cmsg->cmsg_type = IPV6_RECVERR;
+					break;
+				default:
+					assert(0);
+			}
+
+			cmsg->cmsg_len = CMSG_LEN(sizeof (err));
+
+			memset(&err, 0, sizeof (err));
+			err.ee_errno = ENOMSG;
+			err.ee_info = SCM_TSTAMP_SND;
+			err.ee_origin = SO_EE_ORIGIN_TIMESTAMPING;
+
+			memcpy(CMSG_DATA(cmsg), &err, sizeof (err));
 		}
 	}
 #endif

commit b8dafb3faec9a7861a236cff1741b5b0e7969ac0

allow ambiguous ip_family for IP_RECVERR cmsg
This is needed by ptp4l, which uses an AF_INET socket for the
SIOCETHTOOL ioctl even when only UDPv6 transport is configured.

Fixes: 59e55cd801c3 ("add IP/IPV6_RECVERR control message to messages in error queue")

client.c
@@ -2834,16 +2834,15 @@ ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags) {
 			cmsglen += CMSG_SPACE(sizeof (err));
 			assert(msg->msg_control && msg->msg_controllen >= cmsglen);
 			switch (ip_family) {
-				case 4:
-					cmsg->cmsg_level = SOL_IP;
-					cmsg->cmsg_type = IP_RECVERR;
-					break;
 				case 6:
 					cmsg->cmsg_level = SOL_IPV6;
 					cmsg->cmsg_type = IPV6_RECVERR;
 					break;
+				case 4:
 				default:
-					assert(0);
+					cmsg->cmsg_level = SOL_IP;
+					cmsg->cmsg_type = IP_RECVERR;
+					break;
 			}
 
 			cmsg->cmsg_len = CMSG_LEN(sizeof (err));